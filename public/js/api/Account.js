821fce2f771bf9662b4c1a6b32cc8e4cc6a16e76
Andrei Semenov <drew.semenow@gmail.com>
2023-12-16 00:13:27 +0300

    added logic in Account

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,19 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
+  static URL = '/account';
   /**
    * Получает информацию о счёте
    * */
-  static get(id = '', callback){
-
+  static get(callback, id = '') {
+    return createRequest({
+      url: this.URL,
+      method: 'GET',
+      data: {
+        id,
+      },
+      callback,
+    });
   }
 }
+
