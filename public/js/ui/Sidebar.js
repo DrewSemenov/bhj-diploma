bc585a13240a9bf51768fc3e43f87d33da241c10
Andrei Semenov <drew.semenow@gmail.com>
2023-12-16 02:04:40 +0300

    initToggleButton done

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,17 +18,25 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const sidebarMini = document.querySelector('.sidebar-mini');
+    const buttonMenuToggle = document.querySelector('.sidebar-toggle');
 
+    buttonMenuToggle.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      ['sidebar-open', 'sidebar-collapse'].forEach((cls) =>
+        sidebarMini.classList.toggle(cls)
+      );
+    });
   }
 
   /**
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
+   * При нажатии на кнопку регистрации показывает окно регистрации
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
-  static initAuthLinks() {
-
-  }
+  static initAuthLinks() {}
 }
+
